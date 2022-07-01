3ffde64b9de64ef2e3d67e12165321747a3506ce
Xatmo <42683376+Xatmo980@users.noreply.github.com>
2022-07-01 01:17:20 -0700

    Now Updates on CellChange and Level
    Updates on CellChage and Level that way location updates every cell change

--- TopList/TopList.lua
+++ TopList/TopList.lua
@@ -1,12 +1,11 @@
 local Top = {}
 
-customEventHooks.registerHandler("OnPlayerLevel", function(eventStatus, pid)
-    Top = {}
-    local player = Players[pid]
+local UpdatePlayerStats = function(pid)
+      Top = {}
+      local player = Players[pid]
 
-    local playerName = Players[pid].name
-    if (player.data.stats.levelProgress ~= nil) then
-       if (player.data.stats.levelProgress > 0) or (player.data.stats.level > 1) then
+      local playerName = Players[pid].name
+      if (player.data.stats.levelProgress >= 1) or (player.data.stats.level > 1) then
            local LvL = player.data.stats.level
            local Progress = player.data.stats.levelProgress
            local cHP = player.data.stats.healthCurrent
@@ -20,9 +19,21 @@ customEventHooks.registerHandler("OnPlayerLevel", function(eventStatus, pid)
            Top[playerName].healthBase = bHP
            Top[playerName].race = Race
            Top[playerName].class = Class
-           Top[playerName].cell = Cell
+              if tes3mp.IsInExterior(pid) == true  then
+                 local Cell = player.data.location.regionName
+                 Top[playerName].cell = Cell 
+              else
+                 Top[playerName].cell = Cell 
+	      end
 
           jsonInterface.save("TopList/" .. playerName .. ".json", Top)
-    end
-  end
+      end
+end
+
+customEventHooks.registerHandler("OnPlayerLevel", function(eventStatus, pid)
+    UpdatePlayerStats(pid)
+end)
+
+customEventHooks.registerHandler("OnPlayerCellChange", function(eventStatus, pid)
+    UpdatePlayerStats(pid)
 end)
